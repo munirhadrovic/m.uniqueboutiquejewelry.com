a62ab8b81f20a0801faa210ca7b23e29c2f9dfa9
anes89cok <anes89cok@gmail.com>
2015-04-07 14:17:45 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Unique Boutique | Unique Boutique</title>
+		<link rel="stylesheet" href="css/jquery.mobile-1.4.5.css">
+
+		<script src="js/jquery-1.11.2.js"></script>
+		<script>
+		    $(document).on("mobileinit", function(evt) {
+		      // Change some settings
+		    	$.mobile.defaultPageTransition = "flip";
+		    	$.mobile.pageLoadErrorMessage = "Whoops!";
+		    	$.mobile.pageLoadErrorMessageTheme = "b";
+			});
+		</script>
+		<script src="js/jquery.mobile-1.4.5.js"></script>
+		<link rel="stylesheet" href="css/slicknav.css" />
+		<script src="js/jquery.slicknav.js"></script>
+		<script type="text/javascript" src="js/responsiveslides.js"></script>
+		<link rel="stylesheet" href="css/style-mobile.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script>
+			
+			$(function(){
+			   	$( "[data-role='header'], [data-role='footer']" ).toolbar();
+			});	    	
+		   	$(function(){
+				$('#menu').slicknav({
+					prependTo:'#navigation-menu'
+				});
+			});
+			$(function(){
+				$('#menu-contact').slicknav({
+					prependTo:'#navigation-menu-contact'
+				});
+			});
+			$(function(){
+				$('#menu-seo').slicknav({
+					prependTo:'#navigation-menu-seo'
+				});
+			});
+			$(function () {
+
+		      // Slideshow 4
+		      	$("#slider4").responsiveSlides({
+			        auto: true,
+			        pager: false,
+			        nav: true,
+			        speed: 500,
+			        namespace: "callbacks",
+			        before: function () {
+			          $('.events').append("<li>before event fired.</li>");
+			        },
+			        after: function () {
+			          $('.events').append("<li>after event fired.</li>");
+			        }
+		      	});
+
+		    });
+		    $(function () {
+
+		      // Slideshow web
+		      	$("#slider-web").responsiveSlides({
+			        auto: true,
+			        pager: false,
+			        nav: false,
+			        speed: 500,
+			        namespace: "callbacks",
+			        before: function () {
+			          $('.events').append("<li>before event fired.</li>");
+			        },
+			        after: function () {
+			          $('.events').append("<li>after event fired.</li>");
+			        }
+		      	});
+
+		    });
+		   
+			
+			
+    	</script>
+    	
+	</head>
+	<body>
+		
+		<?php 
+			include 'settings.php';
+			
+			
+		?>
+		<div id="header-image" data-role="header" data-theme="<?php echo $theme ?>" data-position="fixed">
+		    <h1><img src="img/logo.png" alt="LOGO"></h1>
+		</div>
+        <div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
+            <h1 style="font-weight: normal; font-size: 13px;">Powered By <a href="http://mobilegrows.com/" rel="external" style="text-decoration: none;">MobileGrows</a> | <a href="http://www.uniqueboutiquejewelry.com/" rel="external" style="text-decoration: none;">  Desktop Site</a></h1>
+        </div>
+		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
+		  
+
+		  <div data-role="main" class="ui-content" data-theme="<?php echo $theme ?>">
+		  		<!-- Jssor Slider Begin -->
+			  	<div class="callbacks_container">
+				    <ul class="rslides" id="slider4">
+				      <li>
+				        <img src="img/slider-images/1slide.jpg" alt="">
+				        <!-- <p class="caption">This is a caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/2slide.jpg" alt="">
+				        <!-- <p class="caption">This is another caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/3slide.jpg" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+                        <li>
+                            <img src="img/slider-images/4slide.jpg" alt="">
+                            <!-- <p class="caption">The third caption</p> -->
+                        </li>
+                        <li>
+                            <img src="img/slider-images/5slide.jpg" alt="">
+                            <!-- <p class="caption">This is a caption</p> -->
+                        </li>
+                        <li>
+                            <img src="img/slider-images/6slide.jpg" alt="">
+                            <!-- <p class="caption">This is another caption</p> -->
+                        </li>
+                        <li>
+                            <img src="img/slider-images/7slide.jpg" alt="">
+                            <!-- <p class="caption">The third caption</p> -->
+                        </li>
+				    </ul>
+			    </div>
+			    <!-- Jssor Slider End -->
+
+              <fieldset class="ui-grid-a" data-theme="<?php echo $theme ?>">
+
+                  <!-- OVDJE SE UPISUJE BROJ TELEFONA KOJI TREBA POZVATI  -->
+
+                  <div class="ui-block-a">
+
+                      <a class="ui-btn ui-icon-phone ui-btn-icon-left buttons-radius" href="#myPopuptelephone" data-rel="popup" data-transition="flow">Call US</a>
+
+                  </div>
+
+                  <!-- ovdje se upisuje grad i adresa iz koje  -->
+
+                  <div class="ui-block-b" id="findUS">
+
+                      <a class="ui-btn ui-icon-location ui-btn-icon-right buttons-radius" href="#myPopup" data-rel="popup" data-transition="flow">Find US</a>
+
+                  </div>
+
+              </fieldset>
+
+
+
+
+
+
+              <!-- pop up for telefon -->
+              <div data-role="popup" id="myPopuptelephone" class="ui-content">
+                  <?php
+                  for ($i=0; $i <$companynumber ; $i++) {
+                      ?>
+                      <a href='tel:<?php echo $telephone[$i] ?>' class="ui-btn"><?php echo $nazivlokacije[$i] ?></a>
+                  <?php
+                  }
+                  ?>
+              </div>
+              <!-- pop up for telefon -->
+
+              <!-- POPUP FOR FIND US -->
+              <div data-role="popup" id="myPopup" class="ui-content">
+                  <div id="multiadress">
+                  </div>
+                  <?php
+                  for ($i=0; $i<$companynumber ; $i++) {
+                      ?>
+                      <script>
+                          var ua = navigator.userAgent;
+                          if(/Android|webOS|Opera Mini/i.test(navigator.userAgent) ) {
+                              console.log("Android uslo");
+                              var prostordugme=document.getElementById('multiadress');
+                              var dugme= document.createElement('a');
+                              dugme.setAttribute('class', "ui-btn findlinks");
+                              dugme.setAttribute('href',"geo:0,0?q=<?php echo $adresa[$i]; ?>,<?php echo $grad[$i]; ?>,<?php echo $skracenica[$i]; ?>");
+                              dugme.innerHTML="<p ><?php echo $nazivlokacije[$i] ?></p>";
+                              prostordugme.appendChild(dugme);
+                          }
+
+                          else if(/iPhone|iPad|iPod/i.test(navigator.userAgent)){
+                              console.log("Iphone ");
+                              var prostordugme=document.getElementById('multiadress');
+                              var dugme= document.createElement('a');
+                              dugme.setAttribute('class', "ui-btn findlinks");
+                              dugme.setAttribute('href',"http://maps.google.com/?daddr=<?php echo $adresa[$i]; ?>,<?php echo $grad[$i]; ?>,<?php echo $skracenica[$i]; ?> ");
+                              dugme.innerHTML="<p><?php echo $nazivlokacije[$i] ?></p>";
+                              prostordugme.appendChild(dugme);
+                          }
+
+                          else if (ua.indexOf("BlackBerry") >= 0)
+                          {
+                              console.log("Blakberu je prosao ");
+                              console.log("Blakberu je prosao ")
+                              var prostordugme=document.getElementById('multiadress');
+                              var dugme= document.createElement('a');
+                              dugme.setAttribute('class', "ui-btn findlinks");
+                              dugme.setAttribute('href',"javascript:blackberry.launch.newMap({'address':{'address1':'<?php echo $adresa[$i]; ?>,<?php echo $grad[$i]; ?>,<?php echo $skracenica[$i]; ?>'}});");
+                              dugme.innerHTML="<p ><?php echo $nazivlokacije[$i] ?></p>";
+                              prostordugme.appendChild(dugme);
+                          }
+
+                          else {
+                              console.log("nije nigdje uslo default ")
+                              var prostordugme=document.getElementById('multiadress');
+                              var dugme= document.createElement('a');
+                              dugme.setAttribute('class', "ui-btn findlinks");
+                              dugme.setAttribute('href',"geo:0,0?q=<?php echo $adresa[$i]; ?>,<?php echo $grad[$i]; ?>,<?php echo $skracenica[$i]; ?>");
+                              dugme.innerHTML="<p ><?php echo $nazivlokacije[$i] ?></p>";
+                              prostordugme.appendChild(dugme);
+                          }
+
+                          // if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                          // // some code..
+                          // }
+                      </script>
+                  <?php
+                  }
+                  ?>
+              </div>
+              <!-- POPUP FOR FIND US -->
+				</fieldset>
+
+              <div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+                  <div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-home ui-btn-icon-left" href="#page2" data-transition="<?php echo $transitionefect ?>">Home</a>
+                  </div>
+              </div>
+						<div data-role="collapsible-set">	
+							<div data-role="collapsible" data-theme="<?php echo $theme ?>" data-content-theme="<?php echo $theme ?>" data-iconpos="right" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d">
+							   <h3>Catalog</h3>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Cape Cod Jewelry</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#seopage" data-transition="<?php echo $transitionefect ?>">Bracelets</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Necklaces</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Pendants</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Earrings</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Rings</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Anklets</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Nautical</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Handbags</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Watches</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Miscellaneous</a>
+
+							</div>
+							<div data-role="collapsible" data-theme="<?php echo $theme ?>" data-content-theme="<?php echo $theme ?>" data-iconpos="right" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d">
+							   <h3>Artists</h3>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Echo of the Dreamer</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Gabriel Ofiesh</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Deborah Armstrong</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">The Mummy's Bundle</a>
+							   <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Craig Hagstrom</a>
+                                <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Bruce London</a>
+                                <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Kitty Sachs</a>
+                                <a class="ui-btn ui-icon-info ui-btn-icon-left buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">Michael Baksa</a>
+							</div>
+						</div>						
+					
+				
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-info ui-btn-icon-left" href="#page2" data-transition="<?php echo $transitionefect ?>">About US</a>
+					</div>
+				</div>
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-info ui-btn-icon-left" href="#page2" data-transition="<?php echo $transitionefect ?>">Guestbook</a>
+					</div>
+				</div>
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-mail ui-btn-icon-left" href="#contactform" data-transition="<?php echo $transitionefect ?>">Contact US</a></div>
+				</div>				   
+		  </div>
+		  
+		 
+		</div>
+		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+		<!-- page 2 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		<div data-role="page" id="page2" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" data-theme="<?php echo $theme ?>">
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu">
+						<ul id="menu">
+							<li><a href="#page1" data-transition="flow">Home</a></li>
+							<li><a href="#page1" data-transition="flip">About us</a></li>
+							<li><a href="#page1" data-transition="fade">Guestbook</a></li>
+							<li><a href="#contactform" data-transition="<?php echo $transitionefect ?>">Contact us</a></li>
+						</ul>
+					</section>
+                    <p class="fntclr">This is only a <b>preview</b> version of your Mobile Website.<br>
+                        Please contact our official Sales Represcutative for Full Functional <b>Version.</b></p>
+					
+
+				</div>
+
+			</div>	
+				
+				
+		</div>
+		
+		<!-- page 2 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+		<!-- contact us ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		
+		<div id="contactform" data-role="page" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" >
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu-contact">
+							<ul id="menu-contact">
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">Home</a></li>
+								<li><a href="#page2" data-transition="pop">About us</a></li>
+								<li><a href="#page2" data-transition="pop">Guestbook</a></li>
+								<li><a href="#page2" data-transition="slide">Contact us</a></li>
+							</ul>
+					</section>
+					<div class="fntclr">
+						<form data-parsley-validate method="post" action="mail.php" data-ajax="false">
+							Name: <input type="text" id="naam" name="naam" value="" placeholder="Name" required/><br>
+							E-mail: <input type="email" id="mail" name="mail" value="" placeholder="E-mail" required/><br>
+							Phone Number: <input type="tel" id="telefoon" name="telefoon" value="" placeholder="Phone"/><br>
+							Subject: <input type="text" id="onderwerp" name="onderwerp" value="" placeholder="Subject" required/><br>
+							Message:<br>
+							<textarea style="height:100px;" id="bericht" name="bericht" placeholder="Enter your message here..." required></textarea> <br />
+							<input type="submit" value="Send" name="submit" class="btn" id="input-dugme">
+						</form>
+					</div>
+				</div>
+			</div>
+		
+		</div>
+<!--contact us///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<!-- page seo ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div id="seopage" data-role="page" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" >
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu-seo">
+							<ul id="menu-seo">
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">Home</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Web Design</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Local SE0</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Web Analytics</a></li>
+							</ul>
+					</section>
+					
+				</div>
+			</div>
+		
+		</div>
+<!--page seo///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div id="succes" data-role="page" data-theme="<?php echo $theme ?>" data-dialog="true">
+		
+			<div data-role="main" class="ui-content" >	
+			 <p>The contact form was sent succesful!</p>
+			
+			</div>
+		
+		</div>
+<!--page succes ends here///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div id="error" data-role="page" data-dialog="true">
+		
+			<div data-role="main" class="ui-content" >	
+				<p>The contact form was sent because of an error!</p>
+			</div>
+		
+		</div>
+<!--erroor ends here////////////////////////////////////////////////////////////////////////////////////////////////////
+	<!-- page 3 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		<!-- SLIDER ZA MOBILNI SAJT POCINJE OVDJEE -->
+		<!-- it works the same with all jquery version from 1.x to 2.x -->
+	
+		<script type="text/javascript" src="js/responsiveslides.js"></script>    
+	
+	   
+		<!-- Slajder se zavrsava -->
+	</body>
+</html>
---
+++ settings.php
@@ -0,0 +1,23 @@
+<?php
+$adresa[0]="204 E. Atlantic Avenue"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="Delray Beach, FL 33444";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Delray Beach";
+$telephone[0]=5612726654;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="1185 Third St. South"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Naples, FL 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Naples";
+$telephone[1]=2396438900;// Broj koji se poziva na dugme CALL US
+
+$adresa[2]="Mashpee Commons 20 North Street"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[2]="Mashpee, MA 02649";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[2]="Cape Cod";
+$telephone[2]=5084773900;// Broj koji se poziva na dugme CALL US
+
+$companynumber=3;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
